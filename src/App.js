b171ff403277ac1e63ca4fdeb974b99c9b14fc53
Rakesh kumar <krakesh.cse@gmail.com>
2022-02-04 16:24:10 -0800

    Keeping clean way of passing render props using children

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import Consumer from "./Consumer";
 export default function App() {
   return (
     <div className="App">
-      {/* <ReusableStateLogic
-        render={(event) => (
-          <Consumer clientX={event.clientX} clientY={event.clientY} />
-        )}
-      /> */}
       <ReusableStateLogic>
         {/**
           Render props passes as children
